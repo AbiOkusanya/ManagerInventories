490df8547e3df1b355ede398ed0042a7c5dc3a83
AbiOkusanya <73357072+AbiOkusanya@users.noreply.github.com>
2021-02-23 13:00:05 -0500

    first commit

---
+++ server/src/main/java/com/example/server/config/DatabaseConfig.java
@@ -0,0 +1,16 @@
+//package com.example.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//@Configuration
+//@EnableWebMvc
+//public class DatabaseConfig implements WebMvcConfigurer  {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
