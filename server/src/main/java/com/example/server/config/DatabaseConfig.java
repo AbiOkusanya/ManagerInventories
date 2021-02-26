d011463f5cd10953fe50a15698873dc766132b36
AbiOkusanya <73357072+AbiOkusanya@users.noreply.github.com>
2021-02-26 17:03:12 -0500

    reversing to redeploy

--- server/src/main/java/com/example/server/config/DatabaseConfig.java
+++ server/src/main/java/com/example/server/config/DatabaseConfig.java
@@ -1,14 +1,16 @@
-package com.example.server.config;
-
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-import javax.sql.DataSource;@Configuration
-public class DatabaseConfig { @Value("${spring.datasource.url}")
-private String dbUrl; @Bean
-public DataSource dataSource() {
-    HikariConfig config = new HikariConfig();
-    config.setJdbcUrl(dbUrl);
-    return new HikariDataSource(config);
-}
-}
+//package com.example.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//@Configuration
+//@EnableWebMvc
+//public class DatabaseConfig implements WebMvcConfigurer  {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
